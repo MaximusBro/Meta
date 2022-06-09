8dac481ca611de9e207b4cab313bd7fa14007a2b
MaximusBro <51210450+MaximusBro@users.noreply.github.com>
2022-06-09 13:55:01 +0300

    animation

--- js/script.js
+++ js/script.js
@@ -10,4 +10,20 @@ function flipflop(id) {
 	if (element)
 		// меняем ему видимость
 		element.style.display = element.style.display == "none" ? "block" : "none";
-} 
+}
+// Smoth scroll and page up
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+
+		const blockID = anchor.getAttribute('href').substr(1)
+
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		})
+	})
+}
+
